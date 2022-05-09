040d09a4f5421e3cd10801dded87dfc8db9be1e9
Douglas Ezra Morrison <dezramorrison@gmail.com>
2022-05-09 13:56:05 -0700

    date label

--- R/postprocess_p_chart.R
+++ R/postprocess_p_chart.R
@@ -5,10 +5,12 @@
 #' @return
 #' @export
 #' @importFrom dplyr mutate rename if_else
+#' @importFrom glue glue
 #'
 postprocess_p_chart = function(
   data,
   digits = 2,
+  date_label = "",
   events = "positive tests",
   trials = "tests",
   proportion = "Positivity rate",
@@ -30,8 +32,9 @@ postprocess_p_chart = function(
         dplyr::if_else(is.na(MIDLINEa), LOWERb, LOWERa),
       `Lower Limit` = pmax(0, `Lower Limit`),
 
-      label = paste0(
-        "# ", events, ": ", n |> format(big.mark = ","),
+      label = glue::glue(
+        "{date_label}{weekdays(date)}, {date}",
+        "\n# ", events, ": ", n |> format(big.mark = ","),
         "\n# ", trials, ": ", N |> format(big.mark = ","),
         "\n", proportion, ": ",round(`Observed %`*multiplier, digits), "%",
         "\nIn Phase #", EPOCH, ": Midline = ", round(Midline*multiplier, digits), "%",
